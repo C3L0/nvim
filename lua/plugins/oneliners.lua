c0aef3a2a55323062f46146c60fbdf3d832b749c
celo <antoine.lopezsimeon@free.fr>
2025-09-04 23:21:09 +0200

    first commit

---
+++ lua/plugins/oneliners.lua
@@ -0,0 +1,6 @@
+-- for plugin without a lot of configuration
+return {
+    { -- Git plugin
+        'tpope/vim-fugitive',
+    },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+		"lua",
+		"c",
+		"python",
+	    },
+	    auto_install = false,
+	})
+    end
+}
