6386756117e80c50c6b6fe157069bcce5557af00
celo <antoine.lopezsimeon@free.fr>
2025-09-05 19:20:22 +0200

    add ruff black in lsp, ² key to swap in normal mode, isort seems to not work

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+-- Insert/Visual/Command mode -> Normal
+vim.keymap.set("i", "²", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "²", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("c", "²", "<Esc>", { noremap = true, silent = true })
+
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,20 +3,53 @@ return {
     dependencies = {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
-        'hrsh7th/nvim-cmp',         -- optional if you want completion
-        'hrsh7th/cmp-nvim-lsp',     -- completion source
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'nvimtools/none-ls.nvim', -- null-ls fork
     },
     config = function()
         require('mason').setup()
         require('mason-lspconfig').setup({
-            ensure_installed = { "pyright" },
+            ensure_installed = {"pyright", "ruff"},
         })
 
         local lspconfig = require('lspconfig')
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-        lspconfig.pyright.setup({
-            capabilities = capabilities,
+        -- LSP servers
+        lspconfig.pyright.setup({ capabilities = capabilities })
+        lspconfig.ruff.setup({ capabilities = capabilities })
+
+        -- Black formatter via none-ls
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort, -- optional
+            },
+        })
+
+        -- Autoformat Python files with Black
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.py",
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+            end,
+        })
+
+        -- Keymaps (same as before)
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                local opts = { buffer = event.buf }
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+                vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', function() vim.lsp.buf.format({ async = true }) end, opts)
+            end,
         })
     end
 }
